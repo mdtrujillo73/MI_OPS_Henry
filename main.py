7c4fdecbb9865866eddcf540d6f3e6c2f1ea042f
mdtrujillo73 <63756937+mdtrujillo73@users.noreply.github.com>
2024-03-17 21:26:52 -0500

    5Consultas

--- main.py
+++ main.py
@@ -22,3 +22,20 @@ def PlayTimeGenre(genre: str):
     max_year = genero_df.loc[genero_df['playtime_forever'].idxmax()]['release_year']
 
     return {"Año de lanzamiento con más horas jugadas para " + genre: int(max_year)}
+
+@app.get("/user_for_genre/{genre}", name='a usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.')
+
+def UserForGenre(genero: str):
+
+    genre_data = user_for_genre[user_for_genre['genre'] == genero]
+
+    if genre_data.empty:
+        return {"Usuario con más minutos jugados para " + genero: None, "minutos jugados": []}
+
+    grouped = genre_data.groupby(genre_data['posted'])['playtime_forever'].sum().reset_index()
+
+    max_playtime_user = genre_data.loc[genre_data['playtime_forever'].idxmax()]['user_id']
+
+    return {
+        "Usuario con más horas jugadas para " + genero: max_playtime_user,
+        "Horas jugadas": [{"Año": year, "minutos": hours} for year, hours in zip(grouped['posted'], grouped['playtime_forever'])]}
