271042b3afa102a712e23681cfc6f10ba8d4ffa7
mdtrujillo73 <63756937+mdtrujillo73@users.noreply.github.com>
2024-03-17 22:17:50 -0500

    PrimerConsulta

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 
 play_genre = pd.read_csv('Funcion_1.csv', low_memory=False)
 user_for_genre = pd.read_csv('Funcion_2_jup.csv', low_memory=False)
+users_recommend = pd.read_csv('Funcion_3_jup.csv', low_memory=False)
 
 @app.get("/release_year/{genre}", name='año con mas horas jugadas para el género ingresado')
 
@@ -45,3 +46,22 @@ def UserForGenre(genero):
         "Horas jugadas": [{"Año": year, "minutos": hours} for year, hours in zip(grouped['posted'], grouped['playtime_forever'])]
     }
     return result
+
+@app.get("/UsersRecommend/{posted}", name='top 3 de juegos MÁS recomendados por usuarios para el año dado.')
+def UsersRecommend(posted):
+    # Filtrar el DataFrame por el año dado
+    df_año = users_recommend[users_recommend['posted'] == posted]
+
+    # Aplicar las condiciones para obtener el top 3
+    top_3_df = df_año[(df_año['sentiment'] >= 0) & (df_año['app_name'].notnull())]
+
+    # Ordenar el DataFrame por 'sentiment' de mayor a menor
+    top_3_df = top_3_df.sort_values(by='sentiment', ascending=False).head(3)
+
+    # Crear una lista de diccionarios con el top 3
+    top_3_dict = {}
+    for i, row in top_3_df.iterrows():
+        puesto = "Puesto " + str(i + 1 - top_3_df.index[0])
+        top_3_dict[puesto] = row['app_name']
+
+    return top_3_dict
