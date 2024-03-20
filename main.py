609c021ff6ded113128dbf45578f9a9ada1b8ca3
mdtrujillo73 <63756937+mdtrujillo73@users.noreply.github.com>
2024-03-20 01:02:47 -0500

    Final

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ users_recommend = pd.read_csv('Funcion_3_jup.csv', low_memory=False)
 users_not_recommend = pd.read_csv('Funcion_4_jup.csv', low_memory=False)
 sentiment_analysis = pd.read_csv('Funcion_5_jup.csv', low_memory=False)
 sentiment_analysis  = sentiment_analysis.drop('Unnamed: 0', axis=1)
-#modelo_svd = joblib.load('modelo_svd_I.pkl')
+modelo_svd = joblib.load('modelo_svd_I.pkl')
 
 @app.get("/release_year/{genre}", name='año con mas horas jugadas para el género ingresado')
 
@@ -93,30 +93,28 @@ def Sentiment_Analysis(release_year: int):
     filas_lista = filas_coincidentes.to_dict(orient='records')
     
     return filas_lista
-#@app.get("/recomendacion_usuario/{user_id}", name='devuelve lista con 5 juegos recomendados para dicho usuario.')
+@app.get("/recomendacion_usuario/{user_id}", name='devuelve lista con 5 juegos recomendados para dicho usuario.')
+def recomendacion_usuario(user_id: object):
+    top=5
 
-
-#def recomendacion_usuario(user_id: object):
-#    top=5
-#
     # Obtener los juegos que el usuario no ha visto aún
-#    games_play = set(df[df['user_id'] == user_id]['item_id'])
-#    games_all = set(df['item_id'])
-#    games_unplay = list(games_all - games_play)
+    games_play = set(df[df['user_id'] == user_id]['item_id'])
+    games_all = set(df['item_id'])
+    games_unplay = list(games_all - games_play)
 
     # Obtener las recomendaciones
-#    predicted_ratings = [modelo_svd.predict(user_id, item_id).est for item_id in games_unplay]
+    predicted_ratings = [modelo_svd.predict(user_id, item_id).est for item_id in games_unplay]
 
     # Ordenar los juegos según su predicción de rating
-#    games_rating = list(zip(games_unplay, predicted_ratings))
-#    games_rating.sort(key=lambda x: x[1], reverse=True)
+    games_rating = list(zip(games_unplay, predicted_ratings))
+    games_rating.sort(key=lambda x: x[1], reverse=True)
 
-#    # Obtener los títulos de los juegos recomendadas
-#    recommended_games = games_rating[:top]
-#    recommended_games = [df[df['item_id'] == item_id]['app_name'].iloc[0] for item_id, _ in recommended_games]
+    # Obtener los títulos de los juegos recomendadas
+    recommended_games = games_rating[:top]
+    recommended_games = [df[df['item_id'] == item_id]['app_name'].iloc[0] for item_id, _ in recommended_games]
 
-#    return {
-#        "A usuarios que son similares a":user_id, 
-#        "también les gustó estos 5 juegos": recommended_games,
-#         
-#    }
+    return {
+        "A usuarios que son similares a":user_id, 
+        "también les gustó estos 5 juegos": recommended_games,
+         
+    }
